337ed5d3bb910187840b47a0b5657769ce2a373b
Rigas <rigas@localhost.localdomain>
2019-08-18 23:05:20 +0100

    More code implemented

--- src/main/scala/com/main/main.scala
+++ src/main/scala/com/main/main.scala
@@ -2,11 +2,6 @@ package com.main
 
 import com.Execution.KafkaZooKeeperExecution
 
-import scala.concurrent.Future
-import scala.concurrent.ExecutionContext
-import ExecutionContext.Implicits.global
-
 object main extends App{
   KafkaZooKeeperExecution.executeKafkaZookeeper
-
 }
